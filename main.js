d0cb70551181e69ffae27c82873ac607b885174e
qwerty2222222 <166916600+qwerty2222222@users.noreply.github.com>
2024-04-25 18:50:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+let button = document.getElementById('click');
+
+button.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.sexText('Вы нажали на кнопку!');
+        tg.MainButton.show()};
+    }
+
+}
